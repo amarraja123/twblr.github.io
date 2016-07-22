c03184c98070a4534c75b66cb50d40e61423e46c
amarraja123 <amar.arawat@gmail.com>
2016-07-22 16:16:28 +0530

    Remove extra var

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,14 +1,13 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-	var num = 0;
-	for i := 0; n != 1; i++ {
+	var i =0;
+	for ; n != 1; i++ {
 		if (n %2 == 0){
 			n = n/2
 		} else {
 			n = ((3 * n) + 1 )
 		}
-		num ++;
 	}
-	return num;
+	return i;
 }
