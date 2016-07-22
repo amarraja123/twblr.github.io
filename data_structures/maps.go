775a6be6d19858b596a9ffd3dc6df1d5fc2ff3d7
amarraja123 <amar.arawat@gmail.com>
2016-07-22 18:00:58 +0530

    Fix for TestMapLikes

--- data_structures/maps.go
+++ data_structures/maps.go
@@ -1,9 +1,24 @@
 package data_structures
 
 func findPeopleWithCommonInterest(data map[string][]string, interest string) []string {
-	return nil
+	result := []string{}
+	for key,value := range data {
+		for _, val := range value{
+			if(interest == val){
+				result = append(result,key)
+			}
+		}
+
+
+	}
+	return result
 }
 
 func contains(src []string, elem string) bool {
+	for _, val := range src{
+		if(elem == val){
+			return true
+		}
+	}
 	return false
 }
