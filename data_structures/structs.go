798f56b32c4bd9f39e75095f08439120a018982c
amarraja123 <amar.arawat@gmail.com>
2016-07-22 20:09:18 +0530

    Fix for TestContainsElementsAddedToLinkedList

--- data_structures/structs.go
+++ data_structures/structs.go
@@ -2,12 +2,54 @@ package data_structures
 
 // Linked List Implementation
 
-type Node struct{}
+type Node struct{
+	data int;
+	next, prev *Node;
 
-func Add(elem int) {
-	return
 }
 
-func Search(elem int) *Node {
-	return nil
+type List struct {
+	head, tail *Node
+	size int
+}
+
+func (l *List) Add(elem int) *List {
+	n := &Node{data: elem}
+	if l.head == nil {
+		l.head = n		// First node
+		l.size = 1;
+	} else {
+		l.tail.next = n	// Add after prev last node
+		n.prev = l.tail // Link back to prev last node
+		l.size++
+	}
+	l.tail = n
+	return l
+}
+
+/**
+Implementation:
+For Example: objectOfList.Search(5)
+ */
+func (l *List) Search(elem int) *Node {
+
+	found := false
+	var ret *Node = nil
+	for n := l.First(); n != nil && !found; n = n.Next() {
+		if n.data == elem {
+			found = true
+			ret = n
+		}
+	}
+	return ret
+}
+
+func (l *List) First() *Node {
+	return l.head
+}
+func (n *Node) Next() *Node {
+	return n.next
+}
+func (n *Node) Prev() *Node {
+	return n.prev
 }
--- data_structures/structs_test.go
+++ data_structures/structs_test.go
@@ -1,15 +1,25 @@
 package data_structures
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestContainsElementsAddedToLinkedList(t *testing.T) {
-	Add(1)
-	Add(2)
-	Add(3)
+	l := new(List);
+
+	l.Add(1)
+	l.Add(2)
+	l.Add(3)
 
 	for i := 1; i <= 3; i++ {
-		if Search(i) == nil {
+		if l.Search(i) == nil {
 			t.Errorf("Should have contained a node with value %d", i)
 		}
 	}
+
+	if l.size != 3 {
+		t.Errorf("The incorrect size is %d", l.size)
+	}
+
 }
+
